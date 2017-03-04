7c21492d905a7f11f6609979a0039b03e0069b6c
Gregor Casar <gregor.casar@xlab.si>
2017-03-04 12:04:48 +0100

    Added websocket for full-duplex communication

--- app.js
+++ app.js
@@ -1,31 +1,70 @@
-var express = require('express')
-var app = express()
+const WEB_PORT = 3000
+const WEBSOCKET_PORT = 8080
+const GPIO_ENABLED = false // set to false to simulate in case not running on raspberry
 
-var last_val = 1
 
+const express = require('express')
+const app = express()
+const gpio = GPIO_ENABLED ? require("gpio") : null;
+const WebSocket = require('ws');
+
+
+//tracks last value of gpio22
+BUTTON_LAST_VALUE = 1 // 1 -> Button not pressed 0 -> Button pressed
+//reserved for websocket connection
+WS_CONNECTION = null;
+
+
+/// SETUP STATIC WEBSERVER //////////////////
 app.use(express.static('public'))
-app.get('/button/', function (req, res) { 
-	var responseObject = {
-		'button':last_val
-	}
-
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify(responseObject))
+app.listen(WEB_PORT, function () {
+  console.log('Static server listening on port '+WEB_PORT)
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
 
-var gpio = require("gpio");
-var gpio22 = gpio.export(22, {
-   direction: "in",
-   ready: function() {
-   }
+/// SETUP FULL-DUPLEX COMMUNICATION /////////
+
+const wss = new WebSocket.Server({ port: WEBSOCKET_PORT });
+console.log('WebSocker server listening on port '+WEBSOCKET_PORT)
+
+wss.on('connection', function connection(ws) {
+  WS_CONNECTION = ws;
+  sendUpdate();
 });
-gpio22.on("change", function(val) {
-   // value will report either 1 or 0 (number) when the value changes
-   console.log(val)
-last_val = val
-});
+
+
+/// SETUP GPIO EVENT LISTENERS /////////////
+
+
+const sendUpdate = function(){
+  if( WS_CONNECTION ){
+    console.log("Client connected, sending first update")
+    const payload = {'button':BUTTON_LAST_VALUE}
+    WS_CONNECTION.send(JSON.stringify(payload))
+  }else{
+    console.log("Warning: no client connected, cannot send update.")
+  }
+}
+
+
+if(GPIO_ENABLED){
+
+  var gpio22 = gpio.export(22, {
+     direction: "in",
+     ready: function() {
+      console.log("GPIO ready")
+     }
+  });
+
+  gpio22.on("change", function(val) {
+    BUTTON_LAST_VALUE = val
+    sendUpdate();
+  });
+
+}else{
+  console.log("GPIO disabled")
+}
+
+
+
 
