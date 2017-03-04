5f40dd218a3f974f04ad744694adf7a93bbf05d2
= <=>
2017-03-04 11:31:49 +0100

    Added support for button toggling via GPIO

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 
+var last_val = 1
+
 app.use(express.static('public'))
 app.get('/button/', function (req, res) { 
 	var responseObject = {
-		'button':1
+		'button':last_val
 	}
 
 	res.setHeader('Content-Type', 'application/json');
@@ -14,3 +16,16 @@ app.get('/button/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+var gpio = require("gpio");
+var gpio22 = gpio.export(22, {
+   direction: "in",
+   ready: function() {
+   }
+});
+gpio22.on("change", function(val) {
+   // value will report either 1 or 0 (number) when the value changes
+   console.log(val)
+last_val = val
+});
+
